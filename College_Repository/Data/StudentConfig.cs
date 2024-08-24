d5e5fd2210b200b96a5a23714be70c469a43ed34
sudhir <sudhirvetri@gmail.com>
2024-08-24 10:31:05 +0000

    Added Tableconfig

---
+++ College_Repository/Data/StudentConfig.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;  
+
+namespace College_Repository.Data
+{
+    public class StudentConfiguration : IEntityTypeConfiguration<Student>
+    {
+      public void Configure(EntityTypeBuilder<Student> builder)
+      {
+                builder.Property(n => n.Name).IsRequired();
+                builder.Property(n => n.Name).HasMaxLength(250);
+                builder.Property(n => n.Email).IsRequired().HasMaxLength(250);
+                builder.Property(n => n.Phone).IsRequired(false).HasMaxLength(12);
+                builder.Property(n => n.AdmissionDate).IsRequired();
+                builder.Property(n => n.DateofBirth).IsRequired();
+
+      } 
+    }
+}
